61aaedb32ee537373be84168666b04bd11cd37c7
Hung <tuanhungpkh@gmail.com>
2023-09-11 13:47:08 +0700

    init project

---
+++ Untitled2.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main() {
+	printf("Hello World!");
+	unsigned int aa=6;
+	//so nguyen duong
+	printf("\ngia tri cua aa = %d",aa);
+	float y=2.5;
+	//so thuc (1.2, 1.3, 1.5 ...)
+	printf("\ngia tri cua y = %f",y);
+	char x ;
+	x='A';
+	//ky tu a-z,A-Z
+	printf("\ngia tri cua x = %c\n",x);
+	
+	return 0;
+}
+
+
+
